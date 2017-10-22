2f7f93ee7ff243ebff79d209ac77d64a98ca154d
innocent <innocent@innoce.nt>
2017-10-22 17:33:43 +0900

    isubata.ruby.service

--- conf/isucon1/default.rb
+++ conf/isucon1/default.rb
@@ -6,3 +6,7 @@ remote_file '/etc/nginx/sites-available/nginx.conf' do
   action :create
   notifies :reload, 'service[nginx]'
 end
+
+remote_file '/etc/systemd/system/isubata.ruby.service' do
+  action :create
+end
